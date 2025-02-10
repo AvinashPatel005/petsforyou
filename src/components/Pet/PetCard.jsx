20643bc483c3182a3223066dae4e2b56fbcfe82c
Avinash <avinashpatel2241016220@gmail.com>
2025-02-10 15:47:48 +0530

    Ui finished for pets

--- src/components/Pet/PetCard.jsx
+++ src/components/Pet/PetCard.jsx
@@ -3,45 +3,43 @@ import { useEffect } from "react";
 
 const PetCard = ({ pet }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  var [yesterday,setYesterday] = useState("");
+  var [yesterday, setYesterday] = useState("");
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
   };
   useEffect(() => {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
     var yyyy = today.getFullYear();
-  
+
     setYesterday(yyyy + '-' + mm + '-' + dd);
   }, [isExpanded]);
 
   return (
-    <div key={pet._id} className={`pet-item ${isExpanded?"expanded":""}`} onClick={toggleExpand}>
+    <div key={pet._id} className={`pet-item ${isExpanded ? "expanded" : ""}`} onClick={toggleExpand}>
       <div className="pet-card-header">
-      <div>
-      <h3>{pet.breed}</h3>
-      <h4>{pet.species}</h4>
-      <p>Age: {pet.age} years</p>
-      <p>Price: ${pet.price}</p>
-      </div>
-      <div>
-        <img src={"https://www.cdc.gov/healthy-pets/media/images/2024/04/GettyImages-598175960-cute-dog-headshot.jpg"} alt="dp" />
-      </div>
+        <div>
+          <h3>{pet.breed}</h3>
+          <p>{pet.gender} {pet.species}</p>
+          <p>Age: {pet.age} years</p>
+          <p className="petcard-desc">{pet.description}</p>
+        </div>
+        <div>
+          <img src={"https://www.cdc.gov/healthy-pets/media/images/2024/04/GettyImages-598175960-cute-dog-headshot.jpg"} alt="dp" />
+        </div>
       </div>
 
       {/* Show additional details when expanded */}
       {isExpanded && (
         <div className="pet-details">
-          <p>Gender: {pet.gender}</p>
-          <p>Category: {pet.category}</p>
-          <p>Health Status: {pet.healthStatus}</p>
+          
+
           <div className="pet-images">
             {pet.images.map((image, index) => (
               <img key={index} src={image} alt={`${pet.breed} ${index + 1}`} />
             ))}
           </div>
-          <p>Description: {pet.description}</p>
           <div className="pet-vaccinations">
             <strong>Vaccinations:</strong>
             <ul>
@@ -50,8 +48,8 @@ const PetCard = ({ pet }) => {
               ))}
             </ul>
           </div>
-          <label htmlFor="date" className="toggle-button" onClick={(e)=>e.stopPropagation()}>Book Appointment
-      </label>
+          <button className="toggle-button" onClick={(e) => e.stopPropagation()}>Book Appointment
+          </button>
         </div>
       )}
 
--- src/components/Pet/PetShopDetails.jsx
+++ src/components/Pet/PetShopDetails.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import Rating from "./Rating";
 
 const PetShopDetails = ({ details, closeDetails, openCataloge }) => {
+  const { ratings: { average, total_reviews } } = details;
   return (
     <div className="petshop-detail-card" key={details.id}>
       <img
         src="https://media.istockphoto.com/id/1355290974/photo/dog-near-different-variation-of-goods-for-animals.jpg?s=612x612&w=0&k=20&c=mL_5zyUinqzo32fKV_0lb0ycD8NnvvlsKCBg51CbO2Q="
         alt={`${details.name}`}
       />
+      <div>
+        
       <div className="petshop-card-content">
         <i className="bi bi-x-circle-fill detail-close-btn" onClick={closeDetails}></i>
-        <h2>{details.name}</h2>
+        <h3>{details.name}</h3>
+
+        <div className="rating-card">
+          <span>{average}</span>
+          <Rating rating={average} />
+          <span>({total_reviews})</span>
+        </div>
+
         <p>
-          <strong>Address:</strong> {details.location.address}, {details.location.city},{" "}
+          {details.location.address}, {details.location.city},{" "}
           {details.location.state}, {details.location.postal_code}
         </p>
         <div>
@@ -19,29 +30,41 @@ const PetShopDetails = ({ details, closeDetails, openCataloge }) => {
           <div className="petshop-hours">
             {Object.keys(details.hours).map((key, index) => (
               <div key={index}>
-                {key.charAt(0).toUpperCase() + key.slice(1)}: {details.hours[key]}
+                {key.charAt(0).toUpperCase() + key.substring(1, 3)}: {details.hours[key]}
               </div>
             ))}
           </div>
         </div>
         <div className="petshop-card-contact">
-          <p>
-            <strong>Contact:</strong>
-          </p>
+          <strong>Contact:</strong>
           <p>Phone: {details.contact.phone}</p>
           <p>
             Email: <a href={`mailto:${details.contact.email}`}>{details.contact.email}</a>
           </p>
-          <p>
-            <a href={details.contact.website} rel="noreferrer" target="_blank">
-              Visit Website
-            </a>
-          </p>
-          <i
-            className="bi bi-arrow-right-circle-fill cataloge-open-btn"
-            onClick={openCataloge}
-          ></i>
+
+
         </div>
+        <div className="petshop-card-contact">
+          <strong>Reviews:</strong>
+          
+          <div className="review-container">
+          {
+            details.reviews.map(({comment,rating,reviewer,date},index)=>(
+              <div className="review-box" key={index}>
+                <h5>{reviewer}</h5>
+                <Rating rating={rating} />
+                <p>{comment}</p>
+                <p className="review-date">{date}</p>
+              </div>
+            ))
+          }
+          </div>
+        </div>
+        <i
+          className="bi bi-arrow-right-circle-fill cataloge-open-btn"
+          onClick={openCataloge}
+        ></i>
+      </div>
       </div>
     </div>
   );
